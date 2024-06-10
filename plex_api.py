ed53cd08629836fb7f1b1a8cf09042da0fedb2d8
Oliver Weidner <61387498+2fersen@users.noreply.github.com>
2024-06-10 12:01:52 +0200

    Add files via upload

--- plex_api.py
+++ plex_api.py
@@ -2,7 +2,7 @@ import time
 import os
 import requests
 import json
-from tmdb_api import add_review
+from tmdb_api import add_rating, add_rating_episode
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=os.path.join('data', '.env'))
@@ -57,31 +57,60 @@ def get_library(lib_key: str):
     return lib
 
 
-def get_metadata(rating_key: str):
-    url = f"{base_url}/library/metadata/{rating_key}"
+def get_metadata(rating_key: str, media_type: str):
+    print(f'[Get_metadata] - rating_key: {rating_key}, media_type: {media_type}')
+    if media_type == 'movie' or media_type == 'episode':
+        url = f"{base_url}/library/metadata/{rating_key}"
 
-    res = requests.get(url, headers=headers, data=payload)
-    f = res.json()['MediaContainer']['Metadata'][0]
-    user_rating = f.get('userRating')
-    if not user_rating:
-        return None, None
+        res = requests.get(url, headers=headers, data=payload)
+        f = res.json()['MediaContainer']['Metadata'][0]
+        user_rating = f.get('userRating')
+        if not user_rating and media_type == 'movie':
+            return None, None
 
-    ids = {"tmdb": None, "imdb": None, "tvdb": None}
-    for i in f['Guid']:
-        idd = i["id"]
-        if "imdb" in idd:
-            ids["imdb"] = idd.replace('imdb://', '')
-        elif "tmdb" in idd:
-            ids["tmdb"] = idd.replace('tmdb://', '')
-        elif "tvdb" in idd:
-            ids["tvdb"] = idd.replace('tvdb://', '')
+        ids = {"tmdb": None, "imdb": None, "tvdb": None}
+        for i in f['Guid']:
+            idd = i["id"]
+            if "imdb" in idd:
+                ids["imdb"] = idd.replace('imdb://', '')
+            elif "tmdb" in idd:
+                ids["tmdb"] = idd.replace('tmdb://', '')
+            elif "tvdb" in idd:
+                ids["tvdb"] = idd.replace('tvdb://', '')
 
-    return user_rating, ids
+        return user_rating, ids
+    else:
+        url = f"{base_url}/library/metadata/{rating_key}/children"
+
+        res = requests.request("GET", url, headers=headers, data=payload).json()["MediaContainer"]['Metadata']
+
+        episode_data = []
+        season_data = []
+        for season in res:
+            print(f'[{rating_key}] - {season["title"]} has {season["leafCount"]}. Episodes')
+            season_rating = season['ratingKey']
+            url = f"{base_url}/library/metadata/{season_rating}/children"
+            s_res = requests.request("GET", url, headers=headers, data=payload).json()
+
+            # if season.get('userRating'):
+            #     user_rating, ids = get_metadata(season_rating, media_type="movie")
+            #     season_data.append([user_rating, ids, season['guid']])
+
+            for episode in s_res['MediaContainer']['Metadata']:
+                if episode.get('userRating'):
+                    season_number = episode['parentIndex']
+                    episode_number = episode['index']
+                    user_rating, ids = get_metadata(episode['ratingKey'], media_type="movie")
+                    episode_data.append([user_rating, ids, episode['guid'], season_number, episode_number])
+                else:
+                    continue
+
+        return episode_data, season_data
 
 
 def get_json_reviews():
     try:
-        with open(os.path.join('data', 'rating_db.json'), 'r') as json_file:
+        with open(os.path.join('data', 'rating_db.json'), 'r', encoding='utf-8') as json_file:
             data = json.load(json_file)
             return data
     except FileNotFoundError:
@@ -106,29 +135,96 @@ def main():
             for i in library:
                 rating_key = i['ratingKey']
                 guid = i['guid']
-                user_rating, ids = get_metadata(rating_key)
 
-                if data[lib_type].get(guid):
-                    if data[lib_type][guid]['tmdb'] == user_rating:
-                        print(f'[{i["title"]}] Already uploaded!')
-                        continue
+                if lib_type == 'movies':
+                    user_rating, ids = get_metadata(rating_key, media_type="movie")
 
-                if user_rating is not None and ids.get('tmdb'):
-                    add_review(ids['tmdb'], user_rating, lib_type)
-                    print(f'[{i["title"]}] Review {user_rating} added!')
+                    if data[lib_type].get(guid):
+                        if data[lib_type][guid]['tmdb'] == user_rating:
+                            print(f'[{i["title"]}] Already uploaded!')
+                            continue
+                        else:
+                            print(f'[{i["title"]}] Rating changed from {data[lib_type][guid]['tmdb']} to {user_rating}')
 
-                    if lib_type == 'show' or lib_type == 'series':
-                        data[lib_type][guid] = {"tmdb": user_rating, "imdb": "", "tvdb": "", "childs": {}}
-                    else:
-                        data[lib_type][guid] = {"tmdb": user_rating, "imdb": "", "tvdb": ""}
-                    time.sleep(0.25)
+                    if user_rating and ids.get('tmdb'):
+                        if add_rating(ids['tmdb'], user_rating, lib_type):
+                            print(f'[{i["title"]}] Review {user_rating} added!')
+
+                            if lib_type == 'show' or lib_type == 'series':
+                                data[lib_type][guid] = {"title": i["title"], "tmdb": user_rating, "imdb": "",
+                                                        "tvdb": "",
+                                                        "episodes": {}, "season": {}}
+                            else:
+                                data[lib_type][guid] = {"title": i["title"], "tmdb": user_rating, "imdb": "",
+                                                        "tvdb": ""}
+                            time.sleep(0.25)
+
+                elif lib_type == 'show' or lib_type == 'series':
+                    _, ids = get_metadata(rating_key, media_type="episode")
+                    episode_data, season_data = get_metadata(rating_key, media_type="series")
+
+                    if not data[lib_type].get(guid):
+                        data[lib_type][guid] = {"title": i["title"], "tmdb": 0, "imdb": 0, "tvdb": 0, "episodes": {},
+                                                "season": {}}
+
+                    for episode in episode_data:
+                        episode_user_rating = episode[0]
+                        episode_ids = episode[1]
+                        episode_guid = episode[2]
+                        season_number = episode[3]
+                        episode_number = episode[4]
+
+                        if data[lib_type].get(guid) and data[lib_type][guid]['episodes'].get(episode_guid):
+                            if data[lib_type][guid]['episodes'][episode_guid]['tmdb'] == episode_user_rating:
+                                print(f'[{i["title"]}] Episode raiting already uploaded!')
+                                continue
+                            else:
+                                print(
+                                    f'[{i["title"]}] Episode rating changed from {data[lib_type][guid]["episodes"]['tmdb']} to {episode_user_rating}')
+
+                        if episode_user_rating and episode_ids.get('tmdb'):
+                            add_rating_episode(ids['tmdb'], episode_user_rating, season_number, episode_number)
+                            print(f'[{i["title"]}] Review {episode_user_rating} added!')
+
+                            data[lib_type][guid]['episodes'][episode_guid] = {"tmdb": episode_user_rating, "imdb": "",
+                                                                              "tvdb": ""}
+                            time.sleep(0.25)
+
+                    # for season in season_data:
+                    #     season_user_rating = season[0]
+                    #     season_ids = season[1]
+                    #     season_guid = season[2]
+                    #
+                    #     if data[lib_type].get(guid) and data[lib_type][guid]['season'].get(season_guid):
+                    #         if data[lib_type][guid]['season'][season_guid]["tmdb"] == season_user_rating:
+                    #             print(f'[{i["title"]}] Episode raiting already uploaded!')
+                    #             continue
+                    #         else:
+                    #             print(
+                    #                 f'[{i["title"]}] Season rating changed from {data[lib_type][guid]['season'][season_guid]['tmdb']} to {season_user_rating}')
+                    #
+                    #     if season_user_rating and season_ids.get('tmdb'):
+                    #         add_review(season_ids['tmdb'], season_user_rating, lib_type)
+                    #         print(f'[{i["title"]}] Review {season_user_rating} added!')
+                    #
+                    #         data[lib_type][guid]['season'][season_guid] = {"tmdb": season_user_rating, "imdb": "",
+                    #                                                        "tvdb": ""}
+                    #         time.sleep(0.25)
                 else:
-                    pass
+                    print(f"library-type: {lib_type}")
+                    raise "Unsuported library type!"
     set_json_reviews(data)
     print('Finished!')
 
 
-print('Starting rating tracker!')
-while True:
-    main()
-    time.sleep(run_interval_in_minutes * 60)
+if __name__ == '__main__':
+    yellow = get_metadata("1301", media_type="movie")
+
+    print('Starting rating tracker!')
+    while True:
+        main()
+        time.sleep(run_interval_in_minutes * 60)
+
+# url = "http://192.168.50.226:32400/library/metadata/1301/children"
+#
+# response = requests.request("GET", url, headers=headers, data=payload).json()
--- tmdb_api.py
+++ tmdb_api.py
@@ -13,7 +13,8 @@ base_url = 'https://api.themoviedb.org'
 api_key = f"Bearer {tmdb_api_key}"
 
 
-def add_review(video_id: str, raiting: str, lib_type: str):
+def add_rating(video_id: str, rating: int | str | float, lib_type: str):
+    print(f'[TMDB-API] - Video-ID: {video_id}, Raiting: {rating}, Library-Type: {lib_type}')
     if lib_type == 'show' or lib_type == 'series':
         lib_type = 'tv'
     elif lib_type == 'movies':
@@ -23,7 +24,7 @@ def add_review(video_id: str, raiting: str, lib_type: str):
 
     url = f"{base_url}/3/{lib_type}/{video_id}/rating"
 
-    payload = "{\"value\"" + f':{raiting}' + "}"
+    payload = "{\"value\"" + f':{rating}' + "}"
     headers = {
         "accept": "application/json",
         "Content-Type": "application/json;charset=utf-8",
@@ -32,8 +33,32 @@ def add_review(video_id: str, raiting: str, lib_type: str):
 
     response = requests.post(url, data=payload, headers=headers)
 
-    # print(response.json()['status_message'])
     if response.status_code == 201:
         return True
     else:
-        return False
+        print(f"TMDB error status message: {response.json()['status_message']}")
+        raise "TMDB API error"
+
+
+def add_rating_episode(video_id: str, rating: int | str | float, season, episode):
+    """
+    https://developer.themoviedb.org/reference/tv-episode-add-rating
+    """
+
+    url = f"{base_url}/3/tv/{video_id}/season/{season}/episode/{episode}/rating"
+
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json;charset=utf-8",
+        "Authorization": api_key
+    }
+
+    payload = "{\"value\"" + f':{rating}' + "}"
+
+    response = requests.post(url, data=payload, headers=headers)
+
+    if response.status_code == 201:
+        return True
+    else:
+        print(f"TMDB error status message: {response.json()['status_message']}")
+        raise "TMDB API error"
